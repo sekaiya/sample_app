8da095e1bd0ba43bf274a2a45137b60a35669aab
sekaiya <sekai.kobayashi@gmail.com>
2013-08-23 17:30:11 +0900

    finish 3

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,34 +2,25 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
   describe "Home page" do
 
-    it "should have the content 'Sample App'" do
+    it "should have the h1 'Sample App'" do
       visit '/static_pages/home'
-      page.should have_content('Sample App')
+      page.should have_selector('h1', :text => 'Sample App')
     end
-    
-    it "should have the right title" do
-		  visit '/static_pages/home'
-		  page.should have_selector('title',
-		                    :text => " | Home")
-		end
 
-  end
-  
-  describe "Help page" do
-
-    it "should have the content 'Help'" do
-      visit '/static_pages/help'
-      page.should have_content('Help')
+    it "should have the base title" do
+      visit '/static_pages/home'
+      page.should have_selector('title',
+                        :text => "Ruby on Rails Tutorial Sample App")
     end
-    
-    it "should have the right title" do
-		  visit '/static_pages/help'
-		  page.should have_selector('title',
-		                    :text => " | Help")
-		end
-		
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| Home')
+    end
   end
   
   describe "About page" do
@@ -42,9 +33,22 @@ describe "Static pages" do
     it "should have the right title" do
 		  visit '/static_pages/about'
 		  page.should have_selector('title',
-		                    :text => " | About Us")
+		                    :text => "#{base_title} | About Us")
 		end
 
+  end
+  
+  describe "Contact" do
+
+    it "should have the content 'Contact'" do 
+      visit '/static_pages/contact'
+      page.should have_content('Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('title', :text => "#{base_title} | Contact")
+    end
   end
   
 end
