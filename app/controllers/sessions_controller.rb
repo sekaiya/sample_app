7a141fde4f879f2cead452271a497a3f43d7006b
sekaiya <sekai.kobayashi@gmail.com>
2013-09-17 13:45:05 +0900

    tmp

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+	  user = User.find_by_email(params[:session][:email].downcase)
+	  if user && user.authenticate(params[:session][:password])
+	    # ユーザをサインインさせ、ユーザページ (show) にリダイレクトする。
+	  else
+      flash.now[:error] = 'Invalid email/password combination' # 誤りあり!
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
+end
